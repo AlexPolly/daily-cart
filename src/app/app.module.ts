044f5048a162dc10524bf5f6ddd0c7659d511b8e
AlexPolly <alexpolly@gmail.com>
2023-12-18 01:11:52 +0530

    dailycart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CartComponent } from './cart/cart.component';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
+    AllProductsComponent,
+    CheckoutComponent,
+    LoginComponent,
+    RegisterComponent,
+    ViewProductComponent,
+    WishlistComponent
   ],
   imports: [
     BrowserModule,
